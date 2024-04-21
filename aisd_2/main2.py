df8fc462e53d86d92571c41f8b076d95079187e5
r1sh11 <ryshi.dixit@gmail.com>
2024-04-21 13:54:06 +0200

    main2 update

--- aisd_2/main2.py
+++ aisd_2/main2.py
@@ -1,55 +1,60 @@
 import sys
 
-def UI():
-    # Jeśli dane zostały przekazane jako wejście za pomocą przekierowania strumienia wejściowego (heredoc)
+def UI(node, insert):
+
+    # Data as heredoc
     if not sys.stdin.isatty():
         input_data = sys.stdin.read().split()
         try:
-            data = [int(x) for x in input_data[1:]]
+            data = [int(x) for x in input_data[0:]]
+            node = ' '.join(str(data[0]))
+            insert = ' '.join(str(x) for x in data[1:])
         except EOFError:
             print("Error reading input.")
-        print("nodes> " + str(input_data[0]))
-        print("insert> " + str(input_data))
-    # W przeciwnym razie, gdy użytkownik wprowadza dane ręcznie
-    else:
+        print("nodes> " + str(node))
+        print("insert> " + str(insert))
+    
+    # user input 
+    elif node == None and insert == []:
         node = input("nodes> ")
         while not node.isdigit():
-            print("Error: expected nodes as integer")
+            print("Error: expected nodes as single integer")
             node = input("nodes> ")
 
         while True:
             insert = input("insert> ")
             input_data = insert.split()
-            try:
-                input_data = [int(num) for num in input_data]
-                break  # Wyjście z pętli, jeśli dane są poprawne
-            except ValueError:
-                print("Error: expected insert as a sequence of numbers separated by a space")
-
-        
-
+            #print(input_data)
+            for x in input_data:
+                if not x.isdigit():
+                    a = False
+                else:
+                    a = True
+            if a == True:
+                break
+            else:
+                print("Error: expected insert as a sequence of integers separated by a single space")
+                    
+        input_data = [int(num) for num in input_data]
         print(input_data)
 
 def main():
-    # Command-line arguments: python script.py --algorithm <algorithm_number>
-    if sys.argv[1] != "--tree": #len(sys.argv) != 3 or 
+    # Command-line arguments: python script.py --tree <tree_type> optional: < *.txt or <<< "data"
+    if sys.argv[1] != "--tree": 
         print("Usage: python script.py --tree <tree_type>")
         sys.exit(1)
 
     tree_type = str(sys.argv[2])
-    
+    node = None
+    insert = []
+
     try: 
         if tree_type != "BST" and tree_type != "AVL":
             raise ValueError("tree_type error: expected value <BST> or <AVL>")
-        UI()
+        else:
+            UI(node, insert)
     except ValueError as e:
         print(e)
-
-    # Perform sorting using the specified algorithm
-    #sorted_data = tree(data, tree_type)
-
-    # Print the sorted data
-    #print("Sorted data:", sorted_data[0:10])
 
 if __name__ == "__main__":
     main()
